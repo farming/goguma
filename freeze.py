f1dd174f16a270ce4f472ab5cf3cafbe895f943b
momamene <momamene@gmail.com>
2013-07-27 01:54:03 +0900

    Revert "Initialize basic flask scaffolding."
    This reverts commit 10534f7a54aa92c1c671f7738b0a25dd1e0079c0.

--- freeze.py
+++
@@ -1,7 +0,0 @@
-from flask_frozen import Freezer
-from goguma import goguma
-
-freezer = Freezer(goguma)
-
-if __name__ == '__main__':
-    freezer.freeze()
--- server.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-from goguma import goguma
-
-goguma.run(host='0.0.0.0', port=5000, debug = True)
