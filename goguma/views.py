f80b2a830960cc90efe1c4a155dfeaeea917cbfc
momamene <momamene@gmail.com>
2013-07-27 03:31:41 +0900

    Show screanshot when post url.

--- goguma/views.py
+++ goguma/views.py
@@ -1,6 +1,23 @@
-from flask import render_template
+from flask import render_template, request
 from goguma import goguma
+from backend.backend import Backend
 
 @goguma.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+@goguma.route('/url', methods=['POST'])
+def maketree():
+    input_url = request.form['url']
+    template = '''
+        <html><head></head>
+        <body>
+        <img alt='page view' src='data:image/png;base64,%s'/>
+        </body>
+        </html>
+        '''
+    with Backend() as b:
+        b.open(input_url)
+        html = template % b.get_base64_image()
+        return html
+        
