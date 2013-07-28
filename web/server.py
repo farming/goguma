412f8c208ffb965d6690d58a5b652d9d4ff989ba
momamene <momamene@gmail.com>
2013-07-28 17:20:01 +0900

    Import socketio and it works well.

--- web/server.py
+++ web/server.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
 from goguma import goguma
+from gevent import monkey
+from socketio.server import SocketIOServer
 
-goguma.run(host='0.0.0.0', port=5000, debug = True)
+monkey.patch_all()
+PORT = 5000
+
+print 'Listening on http://127.0.0.1:%s ' % PORT
+SocketIOServer(('', PORT), goguma, namespace="socket.io", policy_server=False).serve_forever()
+#goguma.run(host='0.0.0.0', port=PORT, debug = True)
