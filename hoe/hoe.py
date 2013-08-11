038ca4be31d3e7f4be1d5e56d866aebed580e650
Juhyeong Park <majecty@gmail.com>
2013-08-11 17:15:14 +0900

    Get a links of pages.
    tasks's get_internal_links get a links of html.
    It doesn't check whether it is a sublink.

--- hoe/hoe.py
+++ hoe/hoe.py
@@ -1,3 +1,5 @@
+from capturer import Capturer
+from probe import Probe
 from selenium import webdriver
 
 class Hoe:
@@ -28,4 +30,10 @@ class Hoe:
         '''
         with open(file_name, 'wb') as f:
             f.write(template % self.image_buffer)
+
+    def get_sub_url(self, base_url, url):
+        probe = Probe()
+        probe.open(url)
+        return probe.get_internal_url()
+
 
