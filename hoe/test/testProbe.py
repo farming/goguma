b059dfae208393499c499c21d913c7fad73f9054
Juhyeong Park <majecty@gmail.com>
2013-08-11 20:56:20 +0900

    Add test code.

---
+++ hoe/test/testProbe.py
@@ -0,0 +1,10 @@
+import unittest
+from probe import Probe
+
+class TestProbe(unittest.TestCase):
+
+    def test_probe(self):
+        self.assertEqual(1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
